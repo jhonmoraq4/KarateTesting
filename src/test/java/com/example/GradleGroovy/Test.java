f0d5700a21c9b440134d0abfedc67538ad40e153
jhonmoraq4 <jhonmoraq4@gmail.com>
2023-06-24 13:09:00 -0500

    5to commit

---
+++ src/test/java/com/example/GradleGroovy/Test.java
@@ -0,0 +1,13 @@
+package com.example.GradleGroovy;
+
+import org.junit.runner.RunWith;
+import com.intuit.karate.junit4.Karate;
+
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import com.intuit.karate.junit5.Karate;
+
+//@ExtendWith(Karate.class)
+@RunWith(Karate.class)
+public class Test {
+
+}
